73dda664dbd377118a4949ca8f350ca7df7b2438
Louise Clark <louise.clark@greyshore.com>
2020-05-20 08:27:35 -0400

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { ThemeProvider } from 'styled-components';
+import { SignUpModal } from './components/Modals';
+import { GlobalStyle, darkTheme, defaultTheme } from "./utils";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    const [useDarkTheme, setUseDarkTheme] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    return(
+        <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+            <button
+        style={{
+          margin: "0 16px 24px",
+          padding: "8px",
+          background: "none",
+          cursor: "pointer",
+          border: "2px solid #000",
+          marginTop: "60px"
+        }}
+        onClick={() => setUseDarkTheme(true)}
+      >
+        Dark theme
+      </button>
+      <button
+        style={{
+          margin: "0 16px 24px",
+          padding: "8px",
+          background: "none",
+          cursor: "pointer",
+          border: "2px solid #000",
+          marginTop: "60px"
+        }}
+        onClick={() => setUseDarkTheme(false)}
+      >
+        Default theme
+      </button>
+      <button
+        style={{
+          margin: "0 16px 24px",
+          padding: "8px",
+          background: "none",
+          cursor: "pointer",
+          border: "2px solid #000",
+          marginTop: "60px"
+        }}
+        onClick={() => setShowModal(!showModal)}
+      >
+       Toggle Modal
+      </button>
+      
+      <div
+        style={{
+          background: useDarkTheme
+            ? defaultTheme.primaryColor
+            : darkTheme.primaryColor,
+          width: "100vw",
+          height: "90vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around"
+        }}
+      >
+        <SignUpModal showModal={showModal} setShowModal={setShowModal} />
+        <GlobalStyle />
+      </div>
+    </ThemeProvider>
+    );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector("#root"));
